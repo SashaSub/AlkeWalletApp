3762451e4d15ea154136ae1b8ee8de5187bf2c32
SashaSub <ssuboch@gmail.com>
2024-05-24 15:16:14 -0400

    Added viewBinding to gradle and Binding to HomePageActivity.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,9 +33,20 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    viewBinding {
+        enable = true
+    }
+    buildFeatures{
+        viewBinding=true
+    }
 }
 
 dependencies {
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
--- app/src/main/java/cl/td/suboch/alkewallet/HomePageActivity.kt
+++ app/src/main/java/cl/td/suboch/alkewallet/HomePageActivity.kt
@@ -8,17 +8,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import cl.td.suboch.alkewallet.databinding.ActivityHomePageBinding
 
 class HomePageActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityHomePageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityHomePageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         val ingreso = findViewById<Button>(R.id.receive_money)
         ingreso.setOnClickListener {
             val ingresarDinero = Intent(this, RequestMoneyActivity::class.java)
