adc2d00f281c94da4b68381b619ca1c4aedbfe96
SashaSub <ssuboch@gmail.com>
2024-05-24 15:37:31 -0400

    Added Binding to SplashScreenActivity.kt

--- app/src/main/java/cl/td/suboch/alkewallet/SplashScreenActivity.kt
+++ app/src/main/java/cl/td/suboch/alkewallet/SplashScreenActivity.kt
@@ -7,19 +7,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import cl.td.suboch.alkewallet.databinding.ActivitySplashScreenBinding
 import java.util.Timer
 import java.util.TimerTask
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+       // binding.logo.setOnClick
+
         //abrir pantalla con un click
         //se declara la imagen como una variable
         /**
          *
-
         val logoApp = findViewById<ImageView>(R.id.logo)
         logoApp.setOnClickListener {
             val abrirPantallaLogin = Intent(this, LoginSignupActivity::class.java)
@@ -30,26 +34,14 @@ class SplashScreenActivity : AppCompatActivity() {
         }
          */
 
-
           //Abrir pantalla con un timer
-        val logoApp = findViewById<ImageView>(R.id.logo)
-                  var task: TimerTask? = object : TimerTask(){
-                      override fun run() {
-                          val abrirPantallaLogin = Intent(baseContext, LoginSignupActivity ::class.java)
-                          abrirPantallaLogin.putExtra("nombre", "Alex")
-                          abrirPantallaLogin.putExtra("apellido", "Suboch")
-                          abrirPantallaLogin.putExtra("acepto_tyC", false)
-                          startActivity(abrirPantallaLogin)
-                      }
-                  }
-                  val timer = Timer()
-                  timer.schedule(task, 2000)
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        var task: TimerTask? = object : TimerTask(){
+            override fun run() {
+                val abrirPantallaLogin = Intent(baseContext, LoginSignupActivity ::class.java)
+                startActivity(abrirPantallaLogin)
+            }
         }
+        val timer = Timer()
+        timer.schedule(task, 2000)
     }
 }
